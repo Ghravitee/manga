7be4d2580ecba37efea5c9277c8b3f79cf827e4f
Ayodele Temitope <dyenamics228@gmail.com>
2024-10-19 20:37:48 +0100

    make more changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,15 @@
 import meme1 from "../assets/meme-1.png";
 import { RiTwitterXFill } from "react-icons/ri";
-import { BiLogoTelegram } from "react-icons/bi";
+import { Typewriter } from "react-simple-typewriter";
 
 const Hero = () => {
   return (
     <div>
       <img src={meme1} alt="" className="mx-auto" />
       <div className="flex flex-col justify-center mt-8 mb-12">
-        <h1 className="text-5xl lg:text-7xl mx-auto">Nah, I&apos;d Win!</h1>
+        <h1 className="text-5xl lg:text-7xl mx-auto mb-12">
+          Nah, I&apos;d Win!
+        </h1>
         <h2 className="text-5xl lg:text-7xl mx-auto">ca: </h2>
       </div>
       <div className="flex gap-8 items-center justify-center my-6 mb-12">
@@ -20,16 +22,6 @@ const Hero = () => {
           data-aos-delay="300"
         >
           <RiTwitterXFill color="black" className="text-6xl " />
-        </a>
-        <a
-          href=""
-          className=""
-          data-aos="zoom-in"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-          data-aos-delay="600"
-        >
-          <BiLogoTelegram color="black" className="text-6xl" />
         </a>
       </div>
       <div className="flex justify-center mt-8 mb-12">
--- src/sections/Memes.jsx
+++ src/sections/Memes.jsx
@@ -1,13 +1,13 @@
-import meme2 from "../assets/meme-2.JPG";
-import meme3 from "../assets/meme-3.JPG";
-import meme4 from "../assets/meme-4.JPG";
-import meme5 from "../assets/meme-5.JPG";
-import meme6 from "../assets/meme-6.JPG";
-import meme7 from "../assets/meme-7.JPG";
+import meme2 from "../assets/meme-2.PNG";
+import meme3 from "../assets/meme-3.PNG";
+import meme4 from "../assets/meme-4.PNG";
+import meme5 from "../assets/meme-5.PNG";
+import meme6 from "../assets/meme-6.PNG";
+import meme7 from "../assets/meme-7.PNG";
 
 const Memes = () => {
   return (
-    <div style={styles.gridContainer} className="py-20">
+    <div className="grid grid-cols-3 gap-[40px] lg:gap-12 py-20">
       <img src={meme2} alt="meme2" style={styles.image} />
       <img src={meme3} alt="meme3" style={styles.image} />
       <img src={meme4} alt="meme4" style={styles.image} />
@@ -19,12 +19,6 @@ const Memes = () => {
 };
 
 const styles = {
-  gridContainer: {
-    display: "grid",
-    gridTemplateColumns: "repeat(3, 1fr)",
-    gap: "20px", // Space between images
-  },
-
   image: {
     width: "100%", // Ensures images fit the grid cells
     height: "fit",
