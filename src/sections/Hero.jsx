12f5655dda3687f0876549a596a6ac4fbdce26e1
Ayodele Temitope <dyenamics228@gmail.com>
2024-10-19 20:51:30 +0100

    add copy

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
 import meme1 from "../assets/meme-1.png";
 import { RiTwitterXFill } from "react-icons/ri";
-import { Typewriter } from "react-simple-typewriter";
+// import { Typewriter } from "react-simple-typewriter";
+import { FaRegCopy } from "react-icons/fa6";
+import { FaCopy } from "react-icons/fa6";
 
 const Hero = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("0x0000000000000000");
+    setHasCopied(true);
+
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
   return (
     <div>
       <img src={meme1} alt="" className="mx-auto" />
       <div className="flex flex-col justify-center mt-8 mb-12">
-        <h1 className="text-5xl lg:text-7xl mx-auto mb-12">
-          Nah, I&apos;d Win!
-        </h1>
-        <h2 className="text-5xl lg:text-7xl mx-auto">ca: </h2>
+        <h1 className="text-5xl lg:text-7xl mx-auto ">Nah, I&apos;d Win!</h1>
+      </div>
+      <div
+        onClick={handleCopy}
+        className="flex justify-center gap-4 items-center"
+      >
+        <h2 className="text-3xl lg:text-6xl">0x0000000000000000 </h2>
+        <div>
+          {hasCopied ? (
+            <FaCopy className="text-3xl lg:text-6xl" />
+          ) : (
+            <FaRegCopy className="text-3xl lg:text-6xl" />
+          )}
+        </div>
       </div>
       <div className="flex gap-8 items-center justify-center my-6 mb-12">
         <a
