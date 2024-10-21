0a1275b431538adb19d0d2427f749eb4e3e760e0
Ayodele Temitope <dyenamics228@gmail.com>
2024-10-21 01:01:45 +0100

    fix audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import { useEffect } from "react";
 import Hero from "./sections/Hero";
 import Memes from "./sections/Memes";
-// import music from "../src/assets/manga.mp3";
 
 const App = () => {
+  useEffect(() => {
+    const unmuteAudio = () => {
+      const audioElement = document.querySelector("audio");
+      if (audioElement && audioElement.muted) {
+        audioElement.muted = false;
+      }
+      // Optionally, remove the event listener after it has been triggered once.
+      window.removeEventListener("click", unmuteAudio);
+      window.removeEventListener("keydown", unmuteAudio);
+      window.removeEventListener("touchstart", unmuteAudio);
+    };
+
+    // Add event listeners for various types of user interactions.
+    window.addEventListener("click", unmuteAudio);
+    window.addEventListener("keydown", unmuteAudio);
+    window.addEventListener("touchstart", unmuteAudio);
+
+    return () => {
+      // Clean up event listeners when the component unmounts.
+      window.removeEventListener("click", unmuteAudio);
+      window.removeEventListener("keydown", unmuteAudio);
+      window.removeEventListener("touchstart", unmuteAudio);
+    };
+  }, []);
+
   return (
     <div>
-      <audio autoPlay loop>
+      {/* Background music with muted initially set to true */}
+      <audio autoPlay loop muted>
         <source src="/manga.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
-      {/* <audio id="audio_tag" src={music} /> */}
 
       <Hero />
       <Memes />
